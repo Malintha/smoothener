6bb5f957c4e2650d1c30afa1ff2fae88f465cd00
Wolfgang Hoenig <whoenig@usc.edu>
2018-02-20 23:39:09 -0800

    add optional argument to read_schedule to only consider a subset of robots

--- utils/read_schedule.m
+++ utils/read_schedule.m
@@ -1,33 +1,42 @@
 % read a schedule from Wolfgang's discrete planner
 % into a [3 x Kpts x Nrobots] waypoint array
 %
-function [paths,names,types] = read_schedule(fname)
+function [paths,names,types] = read_schedule(fname, read_agents)
 	json = loadjson(fname);
-	N = length(json.agents);
+ 
+    if ~exist('read_agents','var')
+        read_agents = 1:length(json.agents);
+    end
+    
+    N = length(read_agents);
+ 
 	k = 0;
-	for i=1:N
+	for i=read_agents
 		k = max(k, length(json.agents{i}.path));
 	end
 	paths = nan(3,k,N);
     names = cell(N,1);
     types = cell(N,1);
-	for i=1:N
+    iprime = 1;
+	for i=read_agents
         %get path
 		p = json.agents{i}.path;
 		len = length(p);
         for j=1:len
-            paths(1,j,i) = str2num(p{j}.x);
-            paths(2,j,i) = str2num(p{j}.y);
-            paths(3,j,i) = str2num(p{j}.z);
+            paths(1,j,iprime) = str2num(p{j}.x);
+            paths(2,j,iprime) = str2num(p{j}.y);
+            paths(3,j,iprime) = str2num(p{j}.z);
         end
         
         for j=(len+1):k
-            paths(:,j,i) = paths(:,len,i);
+            paths(:,j,iprime) = paths(:,len,iprime);
         end
         %get name
-        names{i} = json.agents{i}.name;
+        names{iprime} = json.agents{i}.name;
         %get type
-        types{i} = json.agents{i}.type;
+        types{iprime} = json.agents{i}.type;
+        
+        iprime = iprime + 1;
 	end
 	assert(~any(isnan(paths(:))));
 end
